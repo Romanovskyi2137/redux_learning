90b72102a50619771945946c880e6ba4398a9dfa
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2023-09-13 14:03:00 +0300

    redux basics

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+
+
+function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+  
+  const upCash = (value) => {
+    const action = {
+      type: "UP_CASH",
+      payload: value
+    }; 
+    dispatch(action)
+  };
+  const dropCash = (value) => {
+    const action = {
+      type: "DROP_CASH",
+      payload: value
+    }; 
+    dispatch(action)
+  };
+
+  return (
+      <div className="app-wrapper">
+        <div className="app-container">
+          <div className="App">
+            <h1>{cash}</h1>
+            <button onClick={(e) => upCash(Number(prompt()))} className="button">up cash</button>
+            <button onClick={(e) => dropCash(Number(prompt()))} className="button">drop cash</button>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
