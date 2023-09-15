7e4739bced3c52745f96e3761af0ed4177896ec0
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2023-09-15 10:50:57 +0300

    arrays, action creator fn`s

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-
+import { addClientAction, removeClientAction } from './store/clientReducer';
+// make actionCreator fn
 
 function App() {
   const dispatch = useDispatch();
@@ -23,13 +24,51 @@ function App() {
     dispatch(action)
   };
 
+  const addClient = (value) => {
+    const client = {
+      name: value,
+      id: Date.now()
+    };
+    // const action = {
+    //   type: "ADD_CLIENT",
+    //   payload: client
+    // };
+    dispatch(addClientAction(client))
+  };
+
+  const removeClient = (value) => {
+    // const action = {
+    //   type: "REMOVE_CLIENT",
+    //   payload: value
+    // };
+    dispatch(removeClientAction(value))
+  };
+
+
+  const clientStyle = {
+    border: "1px solid black",
+    borderRadius: "5px",
+    cursor: "pointer",
+    maxWidth: "150px",
+    textAlign: "center",
+    marginTop: "10px"
+  };
+
   return (
       <div className="app-wrapper">
         <div className="app-container">
           <div className="App">
             <h1>{cash}</h1>
-            <button onClick={(e) => upCash(Number(prompt()))} className="button">up cash</button>
-            <button onClick={(e) => dropCash(Number(prompt()))} className="button">drop cash</button>
+            <button onClick={e => upCash(Number(prompt()))} className="button">up cash</button>
+            <button onClick={e => dropCash(Number(prompt()))} className="button">drop cash</button>
+          </div>
+          <button onClick={e => addClient(prompt())}>add client</button>
+          <div>
+              {clients.length > 0 ?
+                  clients.map(client => <div style={clientStyle} key={client.id} onClick={e => removeClient(client.id)}>{client.name}</div>)
+            :
+              <h3>no clients</h3>
+            }
           </div>
         </div>
       </div>
--- src/store/clientReducer.js
+++ src/store/clientReducer.js
@@ -2,13 +2,24 @@ const defaultState = {
     clients: []
   };
 
+const ADD_CLIENT = "ADD_CLIENT";
+const REMOVE_CLIENT = "REMOVE_CLIENT";
+
 export const clinetReducer = (state = defaultState, action) => {
     switch (action.type) {
-      case "ADD_CLIENT":
-        return {...state, cash: state.cash + action.payload};
-      case "REMOVE_CLIENT":
-        return {...state, cash: state.cash - action.payload};
+      case ADD_CLIENT:
+        return {...state, clients: [...state.clients, action.payload]};
+      case REMOVE_CLIENT:
+        return {...state, clients: state.clients.filter(client => client.id !== action.payload)};
       default:
         return state
     }
   };
+
+export const addClientAction = (payload) => {
+    return {type: ADD_CLIENT, payload}
+  };
+
+export const removeClientAction = (payload) => {
+  return {type: REMOVE_CLIENT, payload}
+};
